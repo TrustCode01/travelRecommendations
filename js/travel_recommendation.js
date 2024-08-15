6574afa94493e3db6655d3376093ed8cce11dd37
TrustCode01 <trustcharingani@gmail.com>
2024-08-15 06:30:22 -0400

    adding content to the contact page

--- js/travel_recommendation.js
+++ js/travel_recommendation.js
@@ -1,4 +1,4 @@
-fetch("travel_recommendation.json")
+fetch("travel_recommendation_api.json")
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
